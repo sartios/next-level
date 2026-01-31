1cfd3b10d29805fbc66b7de956b1ba2122732ca9
sartios <917573+sartios@users.noreply.github.com>
2026-01-31 20:53:11 +0200

    Asset database URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,14 @@
+import assert from 'node:assert';
 import { defineConfig } from 'drizzle-kit';
+
+assert(process.env.DATABASE_URL, 'DATABASE_URL is not defined');
 
 export default defineConfig({
   schema: './lib/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL || 'postgresql://nextlevel:nextlevel@localhost:5432/nextlevel'
+    url: process.env.DATABASE_URL
   },
   verbose: true,
   strict: true
