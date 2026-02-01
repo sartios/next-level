4bddaeb0866e17baa6c06dc76c197b12342c234b
angelica <37847644+angelicazgr@users.noreply.github.com>
2026-02-01 10:44:42 +0200

    Remove class

--- app/goal/page.tsx
+++ app/goal/page.tsx
@@ -51,7 +51,7 @@ export default function GoalPage() {
         </div>
       )}
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+      <div>
         <ActiveRoadmap goal={goal} />
       </div>
     </div>
